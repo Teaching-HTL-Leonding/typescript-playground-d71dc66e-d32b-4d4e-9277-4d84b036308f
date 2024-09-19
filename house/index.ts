8045b61668a5db12c0be7bcc473a0ee6b05c214c
Rainer Stropek <rainer@timecockpit.com>
2024-09-19 10:25:03 +0200

    Saved in TypeScript Playground at 2024-09-19T08:25:02.891Z

---
+++ house/index.ts
@@ -0,0 +1,56 @@
+function setup() {
+    // Create the Canvas 
+    // Prameters are width and height
+    createCanvas(1000, 1000);
+
+    // Set background color
+    background("skyblue");
+
+    // Set line color nd thikness (=weight);
+
+    strokeWeight(2);
+
+    // Create the rectangle
+    // Parameterers are width and height
+    fill("red")
+    rect(70, 110, 90, 100);
+
+    // Set line color and thickness (=weight);
+    fill("darkred")
+
+    // Create a triangle
+    // Parameters are width and height
+    triangle(50, 110, 115, 60, 180, 110);
+
+    fill("yellow");
+    rect(80, 160, 50);
+
+    fill("brown");
+    rect(200, 160, 50);
+
+    fill("brown");
+    rect(200, 120, 50);
+
+    fill("brown");
+    rect(200, 80, 50);
+
+    fill("green")
+    circle(190, 100, 100);
+
+    noFill()
+    fill("green");
+    circle(250, 100, 100);
+
+    fill("green")
+    circle(220, 50, 100)
+
+
+
+
+
+
+
+
+
+
+}
