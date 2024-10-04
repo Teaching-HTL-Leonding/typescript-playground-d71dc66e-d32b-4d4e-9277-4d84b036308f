7c99544dbe79aa63f8623b60878a79656199530b
Rainer Stropek <rainer@timecockpit.com>
2024-10-04 12:40:49 +0200

    Saved in TypeScript Playground at 2024-10-04T10:40:49.032Z

---
+++ color_areas/index.ts
@@ -0,0 +1,15 @@
+function setup() {
+    createCanvas(400, 200);
+    background("black");
+    noStroke();
+    fill("blue");
+    rect(0, 0, width/3, height*0.75);
+    fill("red");
+    rect(width/3, 0, height*0.75);
+    fill("yellow");
+    rect(width*2/3, 0, width/3, height*0.75)
+
+    
+}
+
+
