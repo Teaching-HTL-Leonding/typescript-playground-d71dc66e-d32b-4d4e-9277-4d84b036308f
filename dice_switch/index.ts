13b6bed9f3dc3c0febdb5f6c587af5c5e3ce9d1a
Rainer Stropek <rainer@timecockpit.com>
2024-10-23 11:26:06 +0200

    Saved in TypeScript Playground at 2024-10-23T09:26:05.974Z

--- dice_switch/index.ts
+++ dice_switch/index.ts
@@ -1,39 +1,34 @@
 function setup() {
-    const dicePointDiameter: number = 30; // Durchmesser der Punkte
-    createCanvas(200, 200); // Größe des Canvas
-    background("black"); // Hintergrundfarbe
+    const dicePointDiameter: number = 30;
+    createCanvas(200, 200); 
+    background("black"); 
 
-    const dice = Math.floor(random(1, 7)); // Zufällige Zahl zwischen 1 und 6
+    const dice = Math.floor(random(1, 7));
 
-    noStroke(); // Keine Kontur für die Kreise
-    fill("white"); // Farbe der Punkte
+    noStroke();
+    fill("white");
 
-    // Punkte je nach geworfener Zahl zeichnen
+   
     switch (dice) {
         case 1:
-            // Für die 1: einen Punkt in der Mitte
             circle(width / 2, height / 2, dicePointDiameter);
             break;
         case 2:
-            // Für die 2: zwei Punkte diagonal
             circle(width / 4, height / 4, dicePointDiameter);
             circle(3 * width / 4, 3 * height / 4, dicePointDiameter);
             break;
         case 3:
-            // Für die 3: drei Punkte
             circle(width / 4, height / 4, dicePointDiameter);
             circle(width / 2, height / 2, dicePointDiameter);
             circle(3 * width / 4, 3 * height / 4, dicePointDiameter);
             break;
         case 4:
-            // Für die 4: vier Punkte in zwei Reihen
             circle(width / 4, height / 4, dicePointDiameter);
             circle(3 * width / 4, height / 4, dicePointDiameter);
             circle(width / 4, 3 * height / 4, dicePointDiameter);
             circle(3 * width / 4, 3 * height / 4, dicePointDiameter);
             break;
         case 5:
-            // Für die 5: fünf Punkte
             circle(width / 4, height / 4, dicePointDiameter);
             circle(3 * width / 4, height / 4, dicePointDiameter);
             circle(width / 2, height / 2, dicePointDiameter);
@@ -41,7 +36,6 @@ function setup() {
             circle(3 * width / 4, 3 * height / 4, dicePointDiameter);
             break;
         case 6:
-            // Für die 6: sechs Punkte in zwei vertikalen Reihen
             circle(width / 4, height / 4, dicePointDiameter);
             circle(width / 4, height / 2, dicePointDiameter);
             circle(width / 4, 3 * height / 4, dicePointDiameter);
