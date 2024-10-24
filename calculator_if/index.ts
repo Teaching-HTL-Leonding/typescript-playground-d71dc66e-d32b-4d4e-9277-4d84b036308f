8bd61fb7622059127d1c3b97c6beda6e14d067c7
Rainer Stropek <rainer@timecockpit.com>
2024-10-24 09:30:45 +0200

    Saved in TypeScript Playground at 2024-10-24T07:30:45.274Z

--- calculator_if/index.ts
+++ calculator_if/index.ts
@@ -13,17 +13,25 @@ function setup() {
     strokeWeight(3);
     rect(30, 30, 250, 50);
     line(0, lineHeight, width, lineHeight);
-    line(0, lineHeight*2, width, lineHeight*2);
-    line(0, lineHeight*3, width, lineHeight*3);
-    line(0, lineHeight*4, width, lineHeight*4);
-    line(cellWidth, lineHeight, cellWidth, lineHeight*4);
-    line(cellWidth*2, lineHeight, cellWidth*2, lineHeight*5);
+    line(0, lineHeight * 2, width, lineHeight * 2);
+    line(0, lineHeight * 3, width, lineHeight * 3);
+    line(0, lineHeight * 4, width, lineHeight * 4);
+    line(cellWidth, lineHeight, cellWidth, lineHeight * 4);
+    line(cellWidth * 2, lineHeight, cellWidth * 2, lineHeight * 5);
     fill("black");
-    
-    text("7", 50, 130, 200);
-    text("8", 150, 130, 200);
-    text("9", 250, 130, 200);
-    text("6", 250, 220, 100)
+    textSize(75);
+    text("7", 30, 100, 200);
+    text("8", 130, 100, 200);
+    text("9", 230, 100, 200);
+    text("6", 230, 200, 200);
+    text("5", 130, 200, 100);
+    text("4", 30, 200, 100);
+    text("3", 230, 280, 150);
+    text("2", 130, 280, 150);
+    text("1", 30, 280, 150);
+    text("0", 80, 380, 200);
+    text("C", 230, 380, 200);
+
 }
 
 function draw() {
