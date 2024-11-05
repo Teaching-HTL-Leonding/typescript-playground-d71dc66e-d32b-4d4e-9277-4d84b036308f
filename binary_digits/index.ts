567e3945eb7656e3c4f966032e9769b3c0b2ef88
Rainer Stropek <rainer@timecockpit.com>
2024-11-05 20:01:46 +0100

    Saved in TypeScript Playground at 2024-11-05T19:01:46.133Z

--- binary_digits/index.ts
+++ binary_digits/index.ts
@@ -1,35 +1,82 @@
 function setup() {
-  createCanvas(1000, 1000);
-  background("Black");
+  createCanvas(500, 500);
+  background("black");
+  strokeWeight(3);
   stroke("yellow");
-  noFill()
-  rect(50, 100, 50, 100);
-  rect(120, 100, 50, 100);
-  rect(190, 100, 50, 100);
-  rect(260, 100, 50, 100);
-  rect(330, 100, 50, 100);
-  rect(400, 100, 50, 100);
-
-  const num = Math.floor(random(0, 1_000_000));
-  const digit1 = num % 10;
-  const digit2 = Math.floor(num / 10) % 10
-  const digit3 = Math.floor(num / 100) % 10
-  const digit4 = Math.floor(num / 1000) % 10
-  const digit5 = Math.floor(num / 10000) % 10
-  const digit6 = Math.floor(num / 100000) % 10
-
-  textAlign(CENTER, CENTER)
-  fill("yellow");
+  fill("black");
+  rect(50, 100, 50, 80);
   noStroke();
-
-  text(num, width / 2, height / 2)
-  textSize(30);
-
-  text(digit1, 75 + 0 * 75, 100 + 75 / 2);
-  text(digit2, 75 + 1 * 75, 100 + 75 / 2);
-  text(digit3, 75 + 2 * 75, 100 + 75 / 2);
-  text(digit4, 75 + 4 * 75, 100 + 75 / 2);
-  text(digit5, 75 + 5 * 75, 100 + 75 / 2);
-  text(digit6, 75 + 6 * 75, 100 + 75 / 2);
+  fill("yellow");
+  text(erstezahl, 73, 140);
+  noFill();
+  stroke("yellow");
+  rect(120, 100, 50, 80);
+  noStroke();
+  fill("yellow");
+  text(zweitezahl, 143, 140);
+  noFill();
+  stroke("yellow");
+  rect(190, 100, 50, 80);
+  rect(260, 100, 50, 80);
+  rect(330, 100, 50, 80);
+  rect(400, 100, 50, 80);
 }
 
+let erstezahl: number = 1;
+let zweitezahl: number = 1;
+let drittezahl: number = 0;
+let viertezahl: number = 1;
+let fuenftezahl: number = 0;
+let sechstezahl: number = 0;
+
+function mouseClicked() {
+  if (mouseX > 50 && mouseX < 100 && mouseY > 100 && mouseY < 180) {
+    if (erstezahl == 1) {
+      strokeWeight(3);
+      stroke("yellow");
+      fill("black");
+      rect(50, 100, 50, 80);
+
+      erstezahl = 0;
+      noStroke();
+      fill("yellow");
+      text(erstezahl, 73, 140);
+    }
+    else {
+      strokeWeight(3);
+      stroke("yellow");
+      fill("black");
+      rect(50, 100, 50, 80);
+
+      erstezahl = 1;
+      noStroke();
+      fill("yellow");
+      text(erstezahl, 73, 140);
+    }
+  }
+ else{
+   if (mouseX > 120 && mouseX < 170 && mouseY > 100 && mouseY < 180) {
+    if(zweitezahl== 1)
+      strokeWeight(3);
+      stroke("yellow");
+      fill("black");
+    rect(120, 100, 50, 80);
+    noStroke();
+    fill("yellow");
+    zweitezahl = 0;
+    text(zweitezahl, 143, 140);
+  }
+  else {
+      strokeWeight(3);
+      stroke("yellow");
+      fill("black");
+      rect(120, 100, 50, 80);
+
+      zweitezahl = 1;
+      noStroke();
+      fill("yellow");
+      text(zweitezahl, 143, 140);
+    }
+ }
+
+}
