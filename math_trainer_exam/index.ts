5d297cac87f360481c101afdce82e7f835ea7eab
Rainer Stropek <rainer@timecockpit.com>
2024-11-07 08:55:28 +0100

    Saved in TypeScript Playground at 2024-11-07T07:55:28.146Z

--- math_trainer_exam/index.ts
+++ math_trainer_exam/index.ts
@@ -2,12 +2,41 @@ function setup() {
     createCanvas(400, 400);
     background("black");
 
-    
+    const num1 = Math.floor(random(1, 101));
+    const num2 = Math.floor(random(1, 101));
+    const num3 = Math.floor(random(1, 101));
+    const num4= Math.floor (random(1, 101));
+    const num5= Math.floor (random(1, 100));
+    const operator= Math.floor(random(0, 3));
+
+
+    textSize(50);
+    fill("yellow");
+    text(num1, 50, 230);
+    text(num2, 170, 230);
+    text(num3, 290, 230);
+    text(num4, 120, 100);
+    text(num5, 220, 100);
+    if( operator===1){
+        textSize(50);
+        fill("yellow");
+        text("*", 190, 100);
+    }
+    if( operator===2){
+        textSize(50);
+        fill("yellow");
+        text("+", 190, 100)
+    }
+    if( operator===0){
+        textSize(50);
+        fill("yellow")
+        text("-", 190, 90)
+    }
 }
 
 
 function mouseClicked() {
-    
+
 }
 
 function mouseMoved() {
@@ -17,6 +46,6 @@ function mouseMoved() {
 
     fill("white");
     textSize(10);
-    textAlign(LEFT, BOTTOM); 
+    textAlign(LEFT, BOTTOM);
     text(`${mouseX}/${mouseY}`, 5, height - 5);
 }
