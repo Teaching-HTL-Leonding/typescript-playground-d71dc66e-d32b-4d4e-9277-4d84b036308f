eebec08591226e31ac22fd38814cecc112ecfb25
Rainer Stropek <rainer@timecockpit.com>
2024-11-07 20:43:32 +0100

    Saved in TypeScript Playground at 2024-11-07T19:43:32.165Z

--- empty_playground/index.ts
+++ empty_playground/index.ts
@@ -1,10 +1,28 @@
+let num1 = Math.floor(random(1, 101));
+let num2 = Math.floor(random(1, 101));
+let num3 = Math.floor(random(1, 101));
+let position = 0
+let answer = 0
+
 function setup() {
     createCanvas(500, 500);
-    background("sea blue");}
+    background("black");
+    const erstezahl = Math.floor(random(1, 101));
+    const zweitezahl = Math.floor(random(1, 101));
+    let num1 = Math.floor(random(1, 101));
+    let num2 = Math.floor(random(1, 101));
+    let num3 = Math.floor(random(1, 101));
+    let position = 0
+    let answer = 0
+    fill("yellow");
+    textSize(50);
+    text(erstezahl, 100, 100);
+    text(zweitezahl, 300, 100);
+    text(num1, 50, 300);
+    text(num2, 200, 300);
+    text(num3, 350, 300);
+
     
-function draw() {
 }
-//if Statement
-//if(expresion){ code block
-//   }
-//expression kann true, false seinb
+
+
