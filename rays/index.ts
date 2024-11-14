4dd42613d0a30b20118d0c82f9b14bbba7c6cd89
Rainer Stropek <rainer@timecockpit.com>
2024-11-14 08:56:29 +0100

    Saved in TypeScript Playground at 2024-11-14T07:56:28.894Z

--- rays/index.ts
+++ rays/index.ts
@@ -1,21 +1,24 @@
-const SIZE = 400;  
-const MARGIN = 25; 
+const SIZE = 400;
+const MARGIN = 25;
 
 function setup() {
   createCanvas(SIZE, SIZE);
   background("black");
+  strokeWeight(1)
 
+  stroke("yellow");
+  let i = MARGIN;
+  while (i < SIZE-MARGIN) {
+    line(MARGIN, i, mouseX, mouseY);
+    line(width - MARGIN, i, mouseX, mouseY);
 
-    let i = MARGIN;
-    while (i < SIZE) {
-        line(i, mouseY, i, mouseX);
-        i += MARGIN;
-    }
+    i += MARGIN;
+  }
 }
 
 function mouseMoved() {
   background("black");
   strokeWeight(2);
 
- 
+
 }
