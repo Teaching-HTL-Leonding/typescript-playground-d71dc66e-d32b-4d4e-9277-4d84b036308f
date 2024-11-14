3747fe37486e79b7920211badd6436d49fcabb54
Rainer Stropek <rainer@timecockpit.com>
2024-11-14 10:21:18 +0100

    Saved in TypeScript Playground at 2024-11-14T09:21:17.794Z

--- zebra_stripes/index.ts
+++ zebra_stripes/index.ts
@@ -1,48 +1,17 @@
 function setup() {
-    const SIZE = 400;  
+    const SIZE = 400; 
     const STRIPE_THICKNESS = 50; 
 
     createCanvas(SIZE, SIZE);
     background("black");
 
-    noStroke();
-
-    let i=STRIPE_THICKNESS;
+    let i=0;
     while(i<SIZE){
-        fill("green");
-        rect(0, 0, i, 50);
-        i+=25;
-                fill("yellow");
-        rect(0, 50, i, 50);
-        i+=25;
-                fill("green");
-        rect(0, 100, i, 50);
-        i+=25;
-                fill("yellow");
-        rect(0, 150, i, 50);
-        i+=25;
-                fill("green");
-        rect(0, 200, i, 50);
-        i+=25;
-                fill("yellow");
-        rect(0, 250, i, 50);
-        i+=25;
-                fill("green");
-        rect(0, 300, i, 50);
-        i+=25;
-                fill("yellow");
-        rect(0, 350, i, 50);
-        i+=25;
-                fill("green");
-        rect(i, 0, i, 50);
-        i+=25;
-                fill("yellow");
-        rect(i, 50, i, 50);
-        i+=25;
-                fill("green");
-        rect(i, 100, i, 50);
-        i+=25;
+        fill("lime");
+        rect(0, i, SIZE, STRIPE_THICKNESS);
+        i+=50;
+         fill("yellow");
+        rect(0, i, SIZE, STRIPE_THICKNESS);
+        i+=50;
     }
-
-   
 }
