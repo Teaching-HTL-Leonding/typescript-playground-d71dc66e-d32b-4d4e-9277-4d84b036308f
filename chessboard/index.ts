9f17cb5603316605c4fc48505a91ff8c2b265f55
Rainer Stropek <rainer@timecockpit.com>
2024-11-28 10:25:37 +0100

    Saved in TypeScript Playground at 2024-11-28T09:25:36.905Z

--- chessboard/index.ts
+++ chessboard/index.ts
@@ -6,22 +6,33 @@ function setup() {
     background("black");
 
     let i = SQUARE_SIZE;
+    let row = 0;
+
     while (i < 450) {
-        fill("white");
-        rect(i * 2, SQUARE_SIZE * 2, SQUARE_SIZE, SQUARE_SIZE);
-        i += 50
 
         let x = SQUARE_SIZE;
-        while (x < 450) {
-            fill("white");
-            rect(x, SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE);
-            x += SQUARE_SIZE * 2;
+        for (let x = SQUARE_SIZE; x < 450; x += SQUARE_SIZE * 2) {
+            if (row % 2 === 0) {
+                fill("white");
+            }
+            else {
+                fill("brown");
+            }
+            rect(x, i, SQUARE_SIZE, SQUARE_SIZE);
+            
         }
-        let y = SQUARE_SIZE;
-        while (y < 450) {
-            fill("brown");
-            rect(y * 2, SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE);
-            y += SQUARE_SIZE;
+        
+        for(let y = SQUARE_SIZE; y < 450;  y += SQUARE_SIZE) {
+            if (row % 2 === 1) {
+                fill("white");
+            }
+            else {
+                fill("brown");
+            }
+            rect(y * 2, i, SQUARE_SIZE, SQUARE_SIZE);
+            
         }
+        i += SQUARE_SIZE;
+        
     }
 }
