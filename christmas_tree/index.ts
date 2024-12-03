00cf5e7c599ffdb8d002b9f438a0bb4ab8bc5870
Rainer Stropek <rainer@timecockpit.com>
2024-12-03 09:35:32 +0100

    Saved in TypeScript Playground at 2024-12-03T08:35:32.065Z

--- christmas_tree/index.ts
+++ christmas_tree/index.ts
@@ -1,7 +1,11 @@
 const TOP_MARGIN = 80;
 const SPHERE_DIAMETER = 30;
 const INITIAL_TRIANGLE_BASE_WIDTH = 100;
+const SIZE_INCREASE_BY_LAYER = 0.5;
+const HEIGHT_TO_WIDTH_RATIO = 0.5;
 const NUMBER_OF_LAYERS = 4;
+const TRUNK_WIDTH = 50;
+const TRUNK_HEIGHT = 50;
 
 function setup() {
   createCanvas(500, 500);
@@ -9,30 +13,12 @@ function setup() {
   fill("green");
   noStroke();
 
-  for (let pass = 0; pass < 2; pass++) {
-    let triangleBaseWidth = INITIAL_TRIANGLE_BASE_WIDTH;
-    let triangleHeight = triangleBaseWidth / 2;
-
-    resetMatrix();
-    translate(width / 2, TOP_MARGIN + triangleHeight);
-
-    for (let i = 0; i < NUMBER_OF_LAYERS; i++) {
-      if (pass === 0) {
-        triangle(-triangleBaseWidth / 2, 0, 0, -triangleHeight, triangleBaseWidth / 2, 0);
-      } else {
-        fill(Math.floor(random(0, 255)), Math.floor(random(0, 255)), Math.floor(random(0, 255)));
-        circle(-triangleBaseWidth / 2, SPHERE_DIAMETER / 2, SPHERE_DIAMETER);
-        circle(triangleBaseWidth / 2, SPHERE_DIAMETER / 2, SPHERE_DIAMETER);
-      }
-
-      if (i < NUMBER_OF_LAYERS - 1) {
-        translate(0, triangleHeight);
-        triangleBaseWidth *= 1.5;
-        triangleHeight = triangleBaseWidth / 2;
-      }
+    
+  	//triangle(150, 50, 100, 150, 200, 150);
+    let i=INITIAL_TRIANGLE_BASE_WIDTH
+    while(i<500){
+      triangle(150, TRUNK_WIDTH, i, 150, i*2, 300);
+      fill("green");
+      i+=1;
     }
-  }
-
-  fill("brown");
-  rect(-25, 0, 50, 50);
 }
