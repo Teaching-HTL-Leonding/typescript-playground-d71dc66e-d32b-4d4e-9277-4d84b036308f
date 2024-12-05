20ec4a17050d9d2ffd7241ad4d3b8fcbb6b3c134
Rainer Stropek <rainer@timecockpit.com>
2024-12-05 08:10:21 +0100

    Saved in TypeScript Playground at 2024-12-05T07:10:20.878Z

---
+++ patterns_with_lines/index.ts
@@ -0,0 +1,20 @@
+function setup() {
+    createCanvas(601, 301);
+    background("black");
+
+    fill("aqua");
+    noStroke();
+
+    textSize(50);
+    textAlign(CENTER, CENTER);
+    text("Move the mouse to start", 0, 0, width, height);
+}
+
+function mouseMoved() {
+    resetMatrix();
+    background("black");
+    strokeWeight(2);
+
+    noFill();
+    stroke("aqua");
+}
