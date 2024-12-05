85d64341dcb94c189d9654f416be9219b6c89e45
Rainer Stropek <rainer@timecockpit.com>
2024-12-05 08:51:31 +0100

    Saved in TypeScript Playground at 2024-12-05T07:51:30.929Z

--- patterns_with_lines/index.ts
+++ patterns_with_lines/index.ts
@@ -7,14 +7,32 @@ function setup() {
 
     textSize(50);
     textAlign(CENTER, CENTER);
-    text("Move the mouse to start", 0, 0, width, height);
 }
 
+
 function mouseMoved() {
+
+
     resetMatrix();
     background("black");
     strokeWeight(2);
 
     noFill();
     stroke("aqua");
+    let height1 = 0
+    let height2 = 5
+
+    for (let i = 0; i < 601; i++) {
+        for (let x = 0; x < 301; x++) {
+            stroke("aqua");
+            line(0, height1, 10, height2);
+            line(10, height2, 0, height1+10);
+            height1 += 10
+            height2 += 10
+        }
+        translate(10, 0);
+        height1 = 0
+        height2 = 5
+    }
 }
+
