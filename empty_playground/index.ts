b6807a794b1196c41b72307b408b120b0378b17d
Rainer Stropek <rainer@timecockpit.com>
2024-12-10 22:57:30 +0100

    Saved in TypeScript Playground at 2024-12-10T21:57:30.003Z

--- empty_playground/index.ts
+++ empty_playground/index.ts
@@ -1,16 +1,70 @@
+let x = 300;
+let y = 300;
+let speed = 10;
+let ySpeed = 0;
+let gravity = 0.1;
+let ground = 580;
+let canvasSize = 400;
+
+let platformX = 150; 
+let platformY = 300; 
+let platformWidth = 200;
+let platformHeight = 20; 
+let velocityY = 0; 
+let isJumping = 0;
+
+
 function setup() {
-    createCanvas(500, 500);
-    background("grey");
-    textSize(20);
-    text("Test 1", 25, 25);
-    text("Test 2", 100, 25);
-    text("Test 3", 175, 25);
+    createCanvas(5000, 600);
 }
-function mouseMoved(){
-    if(mouseX< 25 && mouseX > 50 &&
-     mouseY < 25 && mouseY > 45){
-        stroke("black");
-        strokeWeight(10)
-        line(25, 25, 20, 20);
+
+function draw() {
+
+    background("green");
+
+    ySpeed += gravity;
+    y += ySpeed;
+
+    if (y >= ground) {
+        y = ground;
+        ySpeed = 0;
     }
+
+    if (keyIsPressed) {
+        if (key === 'W' || key === 'w') {
+            y -= 5;
+        }
+        if (key === 'A' || key === 'a') {
+            x -= 5;
+        }
+        if (key === 'S' || key === 's') {
+            y += 5;
+        }
+        if (key === 'D' || key === 'd') {
+            x += 5;
+        }
+    }
+
+    textSize(50);
+    text("➡️", 200, 100);
+    text("⬅️", 100, 100);
+    text("⬆️", 150, 50);
+    text("⬇️", 150, 100);
+
+    fill("grey");
+    rect(250, 380, 100, 20);
+
+    if(y>380){
+        
+    }
+
+    fill("black");
+    ellipse(x, y, 50, 50);
+    text("Use W,A,S,D to move arround", 50, 150);
+    text("Level 1", 100, 250);
+    stroke("black");
+    line(0, ground, width, ground-100);
+    /*fill("red");
+    rect(0, 480, width, 480);
+    */
 }
