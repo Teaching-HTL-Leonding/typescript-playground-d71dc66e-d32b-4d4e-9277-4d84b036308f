aae055a185211d13b07e11d45eae04e8b8b467ea
Rainer Stropek <rainer@timecockpit.com>
2025-01-03 23:57:24 +0100

    Saved in TypeScript Playground at 2025-01-03T22:57:24.361Z

--- scaled_circles_centered/index.ts
+++ scaled_circles_centered/index.ts
@@ -1,27 +1,25 @@
-let canvasMiddle= 200
-let canvasmid= 100
-
-
 function setup() {
     createCanvas(400, 400);
     background("white");
     noFill();
 
+    translate(150, 150);
+
     stroke("blue");
     strokeWeight(3);
-    circle(canvasMiddle, canvasMiddle, 50);
+    circle(0, 0, 50);
     
     push();
     stroke("red");
     scale(2);
     strokeWeight(3 / 2);
-    circle(canvasmid, canvasmid, 50);
+    circle(0, 0, 50);
     pop();
     
     push();
     stroke("green");
     scale(4.0);
     strokeWeight(3 / 4); 
-    circle(50, 50, 50);
+    circle(0, 0, 50);
     pop();
 }
