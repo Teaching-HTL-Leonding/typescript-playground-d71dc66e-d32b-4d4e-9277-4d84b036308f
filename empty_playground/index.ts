2d28f955b9f1295e3c4f588e1f3b08c7382e83eb
Rainer Stropek <rainer@timecockpit.com>
2025-01-16 08:35:52 +0100

    Saved in TypeScript Playground at 2025-01-16T07:35:51.447Z

--- empty_playground/index.ts
+++ empty_playground/index.ts
@@ -1,74 +1,20 @@
-let x = 300;
-let y = 300;
-let ySpeed = 0;
-let gravity = 0.1;
-let ground = 580;
-let platformHeight = 20;
-let isGameOver = false;
-let img;
-
-let platforms = [
-    { x: 150, y: 400, w: 200 },
-    { x: 400, y: 450, w: 150 },
-    { x: 550, y: 350, w: 150 },
-    { x: 800, y: 450, w: 400 },
-    { x: 1200, y: 350, w: 200 },
-];
-
-let lavaHeight = 20;
-
-function preload() {
-    img = loadImage('https://cddataexchange.blob.core.windows.net/images/space-race/background1.png');
-}
-
 function setup() {
-    createCanvas(5000, 600);
+    createCanvas(400, 400);
+    background("yellow");
+    rect(0, 0, 40, 40);
+    textSize(15);
+    text("🟡", 10, 25);
+    rect(40, 0, 40, 40)
+    text("🟢", 50, 25);
+    rect(80, 0, 40, 40);
+    text("🔴", 90, 25);
 }
 
-function draw() {
-    background(img);
-
-    if (isGameOver) {
-        fill("black");
-        textSize(50);
-        text("Game Over", width / 2 - 150, height / 2);
-        return;
-    }
-
-    ySpeed += gravity;
-    y += ySpeed;
-
-    if (y + 25 > ground) {
-        isGameOver = true;
-    }
-
-    for (let platform of platforms) {
-        if (
-            y + 25 > platform.y &&
-            y + 25 < platform.y + platformHeight &&
-            x > platform.x &&
-            x < platform.x + platform.w &&
-            ySpeed > 0
-        ) {
-            y = platform.y - 25;
-            ySpeed = 0;
-        }
-    }
-
-    if (keyIsPressed) {
-        if (key === "w" || key === "W") y -= 5;
-        if (key === "a" || key === "A") x -= 5;
-        if (key === "d" || key === "D") x += 5;
-    }
-
-    fill("grey");
-    for (let platform of platforms) {
-        rect(platform.x, platform.y, platform.w, platformHeight);
-    }
-
-    fill("red");
-    rect(0, ground, width, lavaHeight);
-
-    fill("black");
-    ellipse(x, y, 50, 50);
+//function mouseClicked(){
+//    if(mouseX <=0 && mouseX >= 40 && mouseY <=0 && mouseY>= 40){
+//        
+//    }
+//}
+function mouseMoved(){
+    
 }
