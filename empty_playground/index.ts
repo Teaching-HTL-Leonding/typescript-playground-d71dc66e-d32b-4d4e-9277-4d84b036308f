c1de23f1f4560abc65c315eebd702020af338bc2
Rainer Stropek <rainer@timecockpit.com>
2025-01-17 12:38:36 +0100

    Saved in TypeScript Playground at 2025-01-17T11:38:35.737Z

--- empty_playground/index.ts
+++ empty_playground/index.ts
@@ -1,20 +1,25 @@
 function setup() {
     createCanvas(400, 400);
-    background("yellow");
-    rect(0, 0, 40, 40);
-    textSize(15);
-    text("🟡", 10, 25);
-    rect(40, 0, 40, 40)
-    text("🟢", 50, 25);
-    rect(80, 0, 40, 40);
-    text("🔴", 90, 25);
+    background("black");
+    for (let i = 0; i < 3; i++) {
+        drawCircle("yellow", "lime");
+    }
+
 }
 
-//function mouseClicked(){
-//    if(mouseX <=0 && mouseX >= 40 && mouseY <=0 && mouseY>= 40){
-//        
-//    }
-//}
-function mouseMoved(){
-    
+function mouseClicked() {
+    for (let i = 0; i < 3; i++) {
+        drawCircle("lime", "yellow");
+
+    }
+}
+
+function drawCircle(color: string, colorStroke : string) {
+    stroke(colorStroke);
+    strokeWeight(4);
+    fill(color);
+    let x = random(0, 400);
+    let y = random(0, 400);
+    let diameter = random(50, 250);
+    circle(x, y, diameter);
 }
