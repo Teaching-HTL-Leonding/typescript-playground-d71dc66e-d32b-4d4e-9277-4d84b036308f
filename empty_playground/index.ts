8663fb769f2f24e65d0bb0213c59396cf1b52b2c
Rainer Stropek <rainer@timecockpit.com>
2025-02-13 20:44:35 +0100

    Saved in TypeScript Playground at 2025-02-13T19:44:34.496Z

--- empty_playground/index.ts
+++ empty_playground/index.ts
@@ -1,25 +1,84 @@
+let circleY = 50;
+let circleX = 250;
+let velocity = 0;
+let gravity = 0.5;
+let jumpPower = -10;
+let speed = 5;
+let onGround = false;
+let offsetX = 0;
+
+let platforms = [
+    { x: 0, y: 400, w: 150, h: 10 },
+    { x: 250, y: 350, w: 150, h: 10 },
+    { x: 500, y: 300, w: 150, h: 10 },
+    { x: 750, y: 250, w: 150, h: 10 },
+    { x: 1000, y: 200, w: 150, h: 10 }
+];
+
 function setup() {
-    createCanvas(400, 400);
-    background("black");
-    for (let i = 0; i < 3; i++) {
-        drawCircle("yellow", "lime");
-    }
-
+    createCanvas(500, 500);
 }
 
-function mouseClicked() {
-    for (let i = 0; i < 3; i++) {
-        drawCircle("lime", "yellow");
-
+function draw() {
+    background("grey");
+    
+    // Gravitation anwenden
+    velocity += gravity;
+    circleY += velocity;
+    
+    // Bewegung mit KeyIsPressed
+    if (keyIsPressed) {
+        if (key === 'a') { // A - links
+            circleX -= speed;
+        }
+        if (key === 'd') { // D - rechts
+            circleX += speed;
+        }
+        if (key === ' ' && onGround) { // Leertaste - springen
+            velocity = jumpPower;
+        }
     }
-}
-
-function drawCircle(color: string, colorStroke : string) {
-    stroke(colorStroke);
-    strokeWeight(4);
-    fill(color);
-    let x = random(0, 400);
-    let y = random(0, 400);
-    let diameter = random(50, 250);
-    circle(x, y, diameter);
+    
+    // Bildschirmbewegung anpassen
+    if (circleX > width - 100) {
+        offsetX -= speed;
+        circleX = width - 100;
+    } else if (circleX < 100) {
+        offsetX += speed;
+        circleX = 100;
+    }
+    
+    // Boden Kollisionsprüfung
+    if (circleY >= height - 25) {
+        circleY = height - 25;
+        velocity = 0;
+        onGround = true;
+    } else {
+        onGround = false;
+    }
+    
+    // Plattform Kollisionsprüfung
+    for (let platform of platforms) {
+        let adjustedX = platform.x + offsetX;
+        if (
+            circleX > adjustedX && 
+            circleX < adjustedX + platform.w && 
+            circleY + 25 >= platform.y && 
+            circleY + 25 <= platform.y + platform.h + velocity
+        ) {
+            circleY = platform.y - 25;
+            velocity = 0;
+            onGround = true;
+        }
+    }
+    
+    // Kreis zeichnen
+    fill("black");
+    circle(circleX, circleY, 50);
+    
+    // Plattformen zeichnen
+    fill("brown");
+    for (let platform of platforms) {
+        rect(platform.x + offsetX, platform.y, platform.w, platform.h);
+    }
 }
